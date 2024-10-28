e7fe144dcdd8cca58fae4037bc133d899f874e5c
PoH <usov.danii22@gmail.com>
2024-10-28 18:52:35 +0300

    conversion

---
+++ scr/conversion_json.py
@@ -0,0 +1,19 @@
+import json
+
+# Исходный JSON
+data = open(input('Введите путь до JSON файла: ')).read()
+
+# Загружаем данные из JSON
+json_data = json.loads(data)
+
+# Конвертируем данные в необходимый формат
+converted_data = []
+for item in json_data["annotations"]:
+    text, annotation = item
+    entities = annotation["entities"]
+    converted_data.append((text, entities))
+
+# Сохраняем результат в training_data.txt
+with open("training_data.txt", "w", encoding="utf-8") as f:
+    for entry in converted_data:
+        f.write(f"{entry}\n")
